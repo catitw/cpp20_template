03fae1bb110d36d0489c188b8e0c8415eccd5d69
catitw <catitw@outlook.com>
2025-01-11 14:43:24 +0800

    update `01_universal_reference_and_collapsing`

--- chapters/function_template/01_universal_reference_and_collapsing.cpp
+++ chapters/function_template/01_universal_reference_and_collapsing.cpp
@@ -1,14 +1,30 @@
+#include <format>
 #include <iostream>
 
 // 万能引用
 // 如果 t 接受左值表达式，则 T 推导为左值引用;
 // 如果 t 接受右值表达式，则 T 推导为右值引用;
-template <typename T> void f(T &&t) {}
+template <typename T> void f(T &&t) {
+  std::cout << std::format("Function signature: {}\n", __PRETTY_FUNCTION__);
+
+  // 判断是左值还是右值
+  if (std::is_lvalue_reference<decltype(t)>::value) {
+    std::cout << "Value category: lvalue\n";
+  } else {
+    std::cout << "Value category: rvalue\n";
+  }
+}
 
 int main() {
   int a = 1;
 
   // 引用折叠： 右值引用的右值引用折叠为右值引用，其余组合均为左值引用.
-  f(a); // T 的类型为 int&, T&& 推导为 int&
-  f(1); // T 的类型为 int&&, T&& 推导为 int&&
+
+  f(a); // 表达式`a`为左值, T 的类型为 int&, T&& 推导为 int&
+  // Function signature: void f(T&&) [with T = int&]
+  // Value category: lvalue
+
+  f(1); // 表达式`1`为右值, T 的类型为 int&&, T&& 推导为 int&&
+  // Function signature: void f(T&&) [with T = int]
+  // Value category: rvalue
 }
